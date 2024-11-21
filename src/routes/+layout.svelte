cda76cfdabb38bd8bac477df33342ba0182bb0be
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-11-21 15:02:23 -0500

    Front page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+
+	:global(html) {
+		font-family: 'Inter', sans-serif;
+		color: var(--foreground);
+		background: var(--background);
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	:root {
+		--background: rgb(255, 252, 240);
+		--foreground: rgb(16, 15, 15);
+		--tx-2: rgb(111, 110, 105);
+		--tx-3: rgb(183, 181, 172);
+	}
+	@media (prefers-color-scheme: dark) {
+		:root {
+			--background: rgb(16, 15, 15);
+			--foreground: rgb(206, 205, 195);
+			--tx-2: rgb(135, 133, 128);
+			--tx-3: rgb(87, 86, 83);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script></script>
+
+<main>
+	<div class="main">
+		<h1>Software Engineer</h1>
+		<p>
+			Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the University of Toronto. Coding and
+			playing the piano are my two main passions. Music, in particular, holds a special place in my
+			heart, allowing me to express myself in unique ways.
+		</p>
+	</div>
+</main>
+
+<style>
+	:root {
+		display: flex;
+		flex-direction: column;
+	}
+	.main {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		/* gap: 1rem; */
+		padding-left: 8rem;
+		padding-right: 8rem;
+	}
+	.main h1 {
+		font-size: 6rem;
+		line-height: 1;
+		font-weight: 400;
+	}
+	.main p {
+		font-size: 1.5rem;
+		line-height: 2rem;
+		font-weight: 300;
+		width: 50%;
+	}
+</style>
