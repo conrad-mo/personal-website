ab9833251790870bc95b50e4d8643e90860adeee
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-11-22 00:18:34 -0500

    holy fuck need to fix screensizing soon

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -32,7 +32,7 @@
 		padding: 0.75rem;
 		margin-left: -0.75rem;
 		margin-right: -0.75rem;
-		max-width: 22rem;
+		max-width: 21rem;
 		transition: background-color 0.3s ease;
 		display: flex;
 		flex-direction: column;
