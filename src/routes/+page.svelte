1b8f413210a19f2f04cadbf521bb2b9a2eef403f
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-11-23 00:25:07 -0500

    Add prerender?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,11 @@
 
 <div class="home">
 	<h1>Software Engineer</h1>
-	<div>
-		<p>Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the University of Toronto.</p>
-		<p>Coding and playing the piano are my two main passions.</p>
-		<p>
-			Music, in particular, holds a special place in my heart, allowing me to express myself in
-			unique ways.
-		</p>
-	</div>
+	<p>
+		Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the University of Toronto. Coding and
+		playing the piano are my two main passions. Music, in particular, holds a special place in my
+		heart, allowing me to express myself in unique ways.
+	</p>
 </div>
 <div class="experience">
 	<h1>Experiences</h1>
