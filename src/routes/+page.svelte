1db95818553dd4bae819fbffa7e6350fdd404d51
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-11-24 15:26:26 -0500

    Fix animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@
 	import { fly } from 'svelte/transition';
 	import { inview } from 'svelte-inview';
 	let homeVisible = $state(false);
+	let expsVisible = $state(false);
+	let projectsVisible = $state(false);
 </script>
 
 <div class="nav">
@@ -14,97 +16,115 @@
 <div
 	use:scrollRef={'home'}
 	class="home"
-	use:inview
+	use:inview={{ threshold: 0.5 }}
 	oninview_enter={() => {
 		homeVisible = true;
 	}}
 >
-	<Typewriter mode="loop"
-		><h1 transition:fly={{ y: 200, duration: 500 }}>Software Engineer</h1>
-		<h1 transition:fly={{ y: 200, duration: 500 }}>Fullstack Developer</h1>
-		<h1 transition:fly={{ y: 200, duration: 500 }}>DevOps Engineer</h1>
-		<h1 transition:fly={{ y: 200, duration: 500 }}>Pianist</h1>
-	</Typewriter>
 	{#if homeVisible}
+		<Typewriter mode="loop"
+			><h1 transition:fly={{ y: 200, duration: 500 }}>Software Engineer</h1>
+			<h1 transition:fly={{ y: 200, duration: 500 }}>Fullstack Developer</h1>
+			<h1 transition:fly={{ y: 200, duration: 500 }}>DevOps Engineer</h1>
+			<h1 transition:fly={{ y: 200, duration: 500 }}>Pianist</h1>
+		</Typewriter>
 		<p transition:fly={{ delay: 150, y: 200, opacity: 0, duration: 500 }}>
 			👋 Hi! I&apos;m Conrad Mo, a Computer Science undergrad at the University of Toronto.
 		</p>
 	{/if}
 </div>
-<div use:scrollRef={'experience'} class="experience">
-	<h1>Experiences</h1>
-	<div class="experience-list">
-		<Experience
-			position="Software Engineer"
-			name="Northern Labs"
-			description="Canadian blockchain startup. Worked on a proprietary newspaper paywall that processes transactions on the blockchain"
-			active={activeExp}
-			photo="/experiences/northernlabs.svg"
-		/>
-		<Experience
-			position="Web Developer"
-			name="Hack the Valley"
-			description="Canada's second largest hackathon"
-			active={activeExp}
-			photo="/experiences/htv.svg"
-		/>
-	</div>
+<div
+	use:scrollRef={'experience'}
+	class="experience"
+	use:inview={{ threshold: 0.5 }}
+	oninview_enter={() => {
+		expsVisible = true;
+	}}
+>
+	{#if expsVisible}
+		<h1 transition:fly={{ y: 200, duration: 500 }}>Experiences</h1>
+		<div transition:fly={{ delay: 150, y: 200, opacity: 0, duration: 500 }} class="experience-list">
+			<Experience
+				position="Software Engineer"
+				name="Northern Labs"
+				description="Canadian blockchain startup. Worked on a proprietary newspaper paywall that processes transactions on the blockchain"
+				active={activeExp}
+				photo="/experiences/northernlabs.svg"
+			/>
+			<Experience
+				position="Web Developer"
+				name="Hack the Valley"
+				description="Canada's second largest hackathon"
+				active={activeExp}
+				photo="/experiences/htv.svg"
+			/>
+		</div>
+	{/if}
 </div>
-<div use:scrollRef={'projects'} class="projects">
-	<h1>Projects</h1>
-	<div class="project-list">
-		<Project
-			name="UTSC Geoguessr"
-			description="A game built with python and tkinter that aims to help new students learn the campus. Submission for HTV 7"
-			photo="utscgeoguessr"
-			link="https://github.com/conrad-mo/geoguesser-utsc"
-		/>
-		<Project
-			name="Retina"
-			description="A desktop app that focuses on helping people explore their
+<div
+	use:scrollRef={'projects'}
+	class="projects"
+	use:inview={{ threshold: 0.5 }}
+	oninview_enter={() => {
+		projectsVisible = true;
+	}}
+>
+	{#if projectsVisible}
+		<h1 transition:fly={{ y: 200, duration: 500 }}>Projects</h1>
+		<div transition:fly={{ delay: 150, y: 200, opacity: 0, duration: 500 }} class="project-list">
+			<Project
+				name="UTSC Geoguessr"
+				description="A game built with python and tkinter that aims to help new students learn the campus. Submission for HTV 7"
+				photo="utscgeoguessr"
+				link="https://github.com/conrad-mo/geoguesser-utsc"
+			/>
+			<Project
+				name="Retina"
+				description="A desktop app that focuses on helping people explore their
                     surroudings by providing definitions of objects around them.
                     Submission for UoftHacksX"
-			photo="retina"
-			link="https://github.com/CherylZ4/Retina"
-		/>
-		<Project
-			name="Viridian"
-			description="A mobile app for iOS and Android that allows users to talk
+				photo="retina"
+				link="https://github.com/CherylZ4/Retina"
+			/>
+			<Project
+				name="Viridian"
+				description="A mobile app for iOS and Android that allows users to talk
                     to each other"
-			photo="viridian"
-			link="https://github.com/conrad-mo/viridian"
-		/>
-		<Project
-			name="Speech Summarizer"
-			description="A mobile app that summarizes long meetings into short
+				photo="viridian"
+				link="https://github.com/conrad-mo/viridian"
+			/>
+			<Project
+				name="Speech Summarizer"
+				description="A mobile app that summarizes long meetings into short
                     meeting summaries. Winner of Best Mobile Hack at Ignition
                     Hacks 2023"
-			photo="speechsummarizer"
-			link="https://github.com/eveev26/SpeechSummarizer"
-		/>
-		<Project
-			name="Personal Website"
-			description="A website to display myself, skills and projects"
-			photo="website"
-			link="https://github.com/conrad-mo/personal-website"
-		/>
-		<Project
-			name="Eventful"
-			description="A mobile app that generates items given an event and
+				photo="speechsummarizer"
+				link="https://github.com/eveev26/SpeechSummarizer"
+			/>
+			<Project
+				name="Personal Website"
+				description="A website to display myself, skills and projects"
+				photo="website"
+				link="https://github.com/conrad-mo/personal-website"
+			/>
+			<Project
+				name="Eventful"
+				description="A mobile app that generates items given an event and
                     optimizes costs of the items for the event. Ranked 14th out
                     of 80 projects at HTV8"
-			photo="eventful"
-			link="https://github.com/conrad-mo/Eventful"
-		/>
-		<Project
-			name="WisdomWhisperer"
-			description="A web app that allows students to upload lecture videos to
+				photo="eventful"
+				link="https://github.com/conrad-mo/Eventful"
+			/>
+			<Project
+				name="WisdomWhisperer"
+				description="A web app that allows students to upload lecture videos to
                     summarize, generate quiz questions, and ask a chatbot for
                     help. Winner of the Student Challenge at NewHacks2023"
-			photo="wisdomwhisperer"
-			link="https://github.com/rtutz/NewHacks2023-WisdomWhisperer"
-		/>
-	</div>
+				photo="wisdomwhisperer"
+				link="https://github.com/rtutz/NewHacks2023-WisdomWhisperer"
+			/>
+		</div>
+	{/if}
 </div>
 
 <style>
@@ -182,7 +202,7 @@
 		border-color: var(--tx-3);
 	}
 	.projects h1 {
-		padding-top: 4rem;
+		margin-top: 6rem;
 		font-size: 3rem;
 		line-height: 1;
 		font-weight: 300;
