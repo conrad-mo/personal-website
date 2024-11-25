2187f080c65bbf750590b1945fd4c39f742e0765
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-11-25 00:01:18 -0500

    Fix preload?

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
-export async function handle({ event, resolve }) {
-	return resolve(event, {
-		preload: ({ type, path }) => {
-			return type === 'font' && path.endsWith('.woff2');
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event, {
+		preload: ({ type }) => {
+			return type === 'font' || type === 'js' || type === 'css';
 		}
 	});
-}
+	return response;
+};
